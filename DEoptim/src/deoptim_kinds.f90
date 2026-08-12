8a2657d2458dd3cb17751e4476fb7e9ca20769fd
Beliavsky <beliavsky@aol.com>
2026-08-12 05:48:49 -0400

    Add DEoptim package

---
+++ DEoptim/src/deoptim_kinds.f90
@@ -0,0 +1,7 @@
+module deoptim_kinds
+    use iso_fortran_env, only : int64, real64
+    implicit none
+    private
+    integer, parameter, public :: dp = real64
+    integer, parameter, public :: i8 = int64
+end module deoptim_kinds
